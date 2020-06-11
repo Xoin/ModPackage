f6b6d9d0d1c6c13cb2aa930d0fbe99a769e3b2dd
xoin <Xoin@users.noreply.github.com>
2020-06-11 18:19:40 +0200

    Basic idea

---
+++ client/modpackage.js
@@ -0,0 +1,70 @@
+let sevenBin = '7zip-bin'
+const Seven = 'node-7z'
+const pathTo7zip = sevenBin.path7za
+let http = require('http');
+let fs = require('fs');
+let runargs = process.argv.slice(2);
+const fetch = require('node-fetch');
+let settingsfile = fs.readFileSync(__dirname + "\\settings.json");
+let settings = JSON.parse(settingsfile);
+
+
+MainMeal();
+async function MainMeal(server) {
+    console.log("Running ModPackage");
+    switch (runargs[0]) {
+        case "install":
+            serverjson = await GetJson("game/" + runargs[1] + "/mod/" + runargs[2]);
+            if (serverjson.status == 200) {
+                console.log(serverjson.latest.filename);
+                download(settings.serverurl + "files/" + runargs[1] + "/" + runargs[2] + "/" + serverjson.latest.filename, serverjson.latest.filename);
+            }
+            else {
+                console.log("nothing there");
+            }
+            break;
+        case "update":
+
+            break;
+        case "remove":
+
+            break;
+        case "package":
+            break;
+        default:
+            break;
+    }
+}
+
+function install(parameter1, parameter2, parameter3) {
+    download("", "test.7z");
+}
+
+function update(parameter1, parameter2, parameter3) {
+    // code to be executed
+}
+
+function remove(parameter1, parameter2, parameter3) {
+    // code to be executed
+}
+
+function download(url, filename) {
+    var file = fs.createWriteStream(filename);
+    var request = http.get(url, function (response) {
+        response.pipe(file);
+    });
+}
+
+async function GetJson(url) {
+    let response = await fetch(settings.serverurl + url);
+    let data;
+    if (response.status == 200) {
+        data = await response.json();
+        data.status = 200;
+    }
+    else {
+        data = { "status": response.status };
+    }
+
+    return data;
+}
---
+++ server/modpackage.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var app = express()
+const port = 3000
+const path = require('path');
+app.use('/files', express.static(path.join(__dirname, 'public')))
+const fs = require('fs');
+const filesFolder = path.join(__dirname, 'public') + '/';
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/test', function (req, res) {
+    let rawdata = fs.readFileSync('student.json');
+    let student = JSON.parse(rawdata);
+    res.send(req.params)
+})
+
+app.get('/games', function (req, res) {
+    const mods = fs.readdirSync(filesFolder);
+
+    res.send(mods)
+})
+
+app.get('/game/:gameName/mod/:modName', function (req, res) {
+    let rawdata = fs.readFileSync(filesFolder + req.params.gameName + '/' + req.params.modName + "/list.json");
+    let list = JSON.parse(rawdata);
+
+    const mods = fs.readdirSync(filesFolder + req.params.gameName + '/' + req.params.modName);
+    mods.sort();
+    list.latest = JSON.parse(fs.readFileSync(filesFolder + req.params.gameName + '/' + req.params.modName + "/" + mods[mods.length - 1]));
+    mods.forEach(file => {
+        if (path.extname(file) == ".json" && file != "list.json") {
+            rawdata = fs.readFileSync(filesFolder + req.params.gameName + '/' + req.params.modName + "/" + file);
+            let data = JSON.parse(rawdata);
+            list.files.push(data);
+        }
+    })
+    res.send(list)
+})
+
+app.listen(port, () => console.log(`Serving files on ${port}!`))
